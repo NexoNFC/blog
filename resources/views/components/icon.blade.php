96bf40e1cc8b00d9273144271d01f480b80d9be0
Erick Pérez <ericksperezc@gmail.com>
2026-09-15 16:13:20 -0500

    Refactor icon component to support dynamic sizing based on user-defined classes. Added logic to determine if explicit size classes are provided, ensuring default sizes are applied only when necessary.

--- resources/views/components/icon.blade.php
+++ resources/views/components/icon.blade.php
@@ -1,5 +1,11 @@
-<svg {{ $attributes->merge([
-    'class' => 'h-6 w-6',
+@php
+    $userClass = trim((string) $attributes->get('class', ''));
+    $hasExplicitSize = $userClass !== '' && preg_match('/(?:^|\s)!?(?:h|w|size)-[^\s]+/', $userClass) === 1;
+    $class = $hasExplicitSize ? $userClass : trim('h-6 w-6 '.$userClass);
+@endphp
+
+<svg {{ $attributes->except('class')->merge([
+    'class' => $class,
     'xmlns' => 'http://www.w3.org/2000/svg',
     'fill' => $icon['fill'],
     'viewBox' => $icon['viewBox'],
